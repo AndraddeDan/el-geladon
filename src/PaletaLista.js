d58e5e7a5d6cc5450cca2d78f467c13cde573e32
Daniel Andrade <andraddedan@icloud.com>
2021-12-13 18:35:04 -0300

    refactor: some style adjusts

--- src/PaletaLista.js
+++ src/PaletaLista.js
@@ -7,10 +7,13 @@ function PaletaLista() {
 		{paletas.map((paleta, index) => 
 			<div className="PaletaListaItem" key={`PaletaListaItem${index}`}>
 				<div>
-				<div className="PaletaListaItem__titulo"> { paleta.titulo } </div>
-				<div className="PaletaListaItem__preco">R$ { paleta.preco.toFixed(2) }</div>
-				<div className="PaletaListaItem__descricao"> { paleta.descricao }  </div>
-				<button className="PaletaListaItem__selecionar">selecionar</button>
+					<div className="PaletaListaItem__titulo"> { paleta.titulo } </div>
+					<div className="PaletaListaItem__preco">R$ { paleta.preco.toFixed(2) }</div>
+					<div className="PaletaListaItem__descricao"> { paleta.descricao }  </div>
+					<div className="PaletaListaItem__acoes Acoes">
+						<button className="Acoes__adicionar Acoes__adicionar--preencher">adicionar</button>
+						{/* <button className="Acoes__remover">remover</button> */}
+					</div>
 				</div>
 				<img className="PaletaListaItem__foto" src={paleta.foto} alt={`Paleta de ${paleta.sabor}`} />
 			</div>
