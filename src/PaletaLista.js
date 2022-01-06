6843179f2d282cf6a241a9bde6f7e87b911b7618
Daniel Andrade <andraddedan@icloud.com>
2022-01-06 01:32:33 -0300

    feat: conditional renderization added

--- src/PaletaLista.js
+++ src/PaletaLista.js
@@ -10,33 +10,31 @@ function PaletaLista() {
 		setQuantidade({ ...paletasSelecionadas, ...paleta});
 	}
 
-	// const removerItem = (paletaIndex) => {
-	// 	const paleta = { [paletaIndex]: Number(paletasSelecionadas[paletaIndex] || 0) -1 }
-	// 	setQuantidade({ ...paletasSelecionadas, ...paleta});
-	// }
+	const removerItem = (paletaIndex) => {
+		const paleta = { [paletaIndex]: Number(paletasSelecionadas[paletaIndex] || 0) -1 }
+		setQuantidade({ ...paletasSelecionadas, ...paleta});
+	}
 
-	// const removeButton = (canRender, index) =>
-	// 	Boolean(canRender) && (<button className="Acoes__remover" onClick={() => removerItem(index)}>remover</button>);
+	const removeButton = (canRender, index) =>
+		Boolean(canRender) && (<button className="Acoes__remover" onClick={() => removerItem(index)}>remover</button>);
 
-	// const badgeCounter = (canRender, index) =>
-	// 	Boolean(canRender) && (<span className="PaletaListaItem__badge"> {paletasSelecionadas[index]} </span>);
+	const badgeCounter = (canRender, index) =>
+		Boolean(canRender) && (<span className="PaletaListaItem__badge"> {paletasSelecionadas[index]} </span>);
 
 	return (
 		<div className="PaletaLista">
 			{paletas.map((paleta, index) => 
 				<div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
-					{/* {badgeCounter(paletasSelecionadas[index], index)} */}
-					<span className="PaletaListaItem__badge"> {paletasSelecionadas[index] || 0} </span>
+					{badgeCounter(paletasSelecionadas[index], index)}
 					<div>
 						<div className="PaletaListaItem__titulo"> { paleta.titulo } </div>
 						<div className="PaletaListaItem__preco">R$ { paleta.preco.toFixed(2) }</div>
 						<div className="PaletaListaItem__descricao"> { paleta.descricao }  </div>
 						<div className="PaletaListaItem__acoes Acoes">
-							<button className="Acoes__adicionar Acoes__adicionar--preencher" onClick={() => adicionarItem(index)}>adicionar</button>
-							{/* <button className={`Acoes__adicionar ${!paletasSelecionadas[index] && "Acoes__adicionar--preencher"}`}
+							<button className={`Acoes__adicionar ${!paletasSelecionadas[index] && "Acoes__adicionar--preencher"}`}
 							onClick={() => adicionarItem(index)}
-							>adicionar</button> */}
-							{/* {removeButton(paletasSelecionadas[index], index)} */}
+							>adicionar</button>
+							{removeButton(paletasSelecionadas[index], index)}
 						</div>
 					</div>
 					<img className="PaletaListaItem__foto" src={paleta.foto} alt={`Paleta de ${paleta.sabor}`} />
